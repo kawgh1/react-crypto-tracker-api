608c10c47e26265e49454b1f1cec04d5995a11ce
J J <krseven@gmail.com>
2022-03-01 22:13:58 -0600

    getCoins from API

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Basic React app using the Coin Gecko API
+
+
+        function App() {
+
+            const [coins, setCoins] = useState([]);
+
+            
+            async function getCoins() {
+                try {
+                    const response = await fetch('api-url-end-point')
+                    const data = await response.json();
+                    setCoins(data);
+                } catch (error) {
+                console.log(error);
+                }
+                
+            }
+
+            useEffect(() => {
+                getCoins();
+            }, [])
+
+            console.log(coins)
+
+            return(...)
+            ...
+        }
--- src/App.js
+++ src/App.js
@@ -10,31 +10,30 @@ import './App.css';
 
 function App() {
 
-  
   const [coins, setCoins] = useState([]);
 
+  
   async function getCoins() {
-  
     try {
-      const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false');
-      const data = await response.json();
-      setCoins(data);
-  
+        const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+        const data = await response.json();
+        setCoins(data);
     } catch (error) {
       console.log(error);
     }
+      
   }
-  
+
   useEffect(() => {
     getCoins();
   }, [])
-  
+
   console.log(coins)
 
 
 
   return (
-    <div className="App">
+    <div className="coin-app">
       <h1></h1>
     </div>
   );
