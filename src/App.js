bcf9b0e2251e4513587bc809e97ab9f9dbc1e386
J J <krseven@gmail.com>
2022-03-01 19:06:29 -0600

    setup getCoins api fetch

--- src/App.js
+++ src/App.js
@@ -3,7 +3,34 @@ import './App.css';
 
 // https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
 
+// https://dog.ceo/api/breeds/list/all
+
+
+
+
 function App() {
+
+  
+  const [coins, setCoins] = useState([]);
+
+  async function getCoins() {
+  
+    try {
+      const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false');
+      const data = await response.json();
+      setCoins(data);
+  
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  useEffect(() => {
+    getCoins();
+  }, [])
+  
+  console.log(coins)
+
 
 
   return (
